0b5c6db19860e8a01aaf1469007e2838f26f0378
OilStone <you8343@gmail.com>
2026-06-01 18:24:26 +0900

    feat: 이동 경로 파티클 추가
      - 말 이동 확정 시 이동 경로를 따라 파티클을 재생
      - DOTween으로 파티클 방출기를 시작 위치에서 도착 위치까지 이동
      - 이동 완료 후 새 방출만 중단하고 남은 파티클은 자연 소멸 처리

--- Assets/Scripts/Runtime/Particle/PooledParticle.cs
+++ Assets/Scripts/Runtime/Particle/PooledParticle.cs
@@ -1,6 +1,7 @@
 
 
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Yujanggi.Runtime.Particle
@@ -10,6 +11,8 @@ namespace Yujanggi.Runtime.Particle
         [SerializeField] private ParticleSystem _particleSystem;
 
         private Action<PooledParticle> _onStopped;
+        private Tween _moveTween;
+
         public void Initialize(Action<PooledParticle> onStopped)
         {
             _onStopped = onStopped;
@@ -32,18 +35,56 @@ namespace Yujanggi.Runtime.Particle
         }
         public void Play(Vector3 worldPosition)
         {
+            CancelMoveTween();
+
             transform.position = worldPosition;
             gameObject.SetActive(true);
 
+            var main = _particleSystem.main;
+            main.loop = false;
             _particleSystem.Clear(true);
             _particleSystem.Play(true);
+        }
+        public void PlayPath(Vector3 from, Vector3 to, float duration, Ease ease)
+        {
+            CancelMoveTween();
+
+            transform.position = from;
+            gameObject.SetActive(true);
+
+            var main = _particleSystem.main;
+            main.loop = true;
+
+            _particleSystem.Clear(true);
+            _particleSystem.Play(true);
+
+            _moveTween = transform
+                .DOMove(to, duration)
+                .SetEase(ease)
+                .OnComplete(() =>
+                {
+                    StopEmitting();
+                    _moveTween = null;
+                });
         }
 
         private void OnParticleSystemStopped()
         {
+            CancelMoveTween();
             _particleSystem.Clear(true);
             gameObject.SetActive(false);
             _onStopped?.Invoke(this);
+        }
+        private void CancelMoveTween()
+        {
+            _moveTween?.Kill();
+            _moveTween = null;
+        }
+        private void StopEmitting()
+        {
+            var main = _particleSystem.main;
+            main.loop = false;
+            _particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmitting);
         }
     }
 }
