880072eeeb5fda0cc4fb81ac13bfaf76e484ea90
OilStone <you8343@gmail.com>
2026-06-01 18:51:08 +0900

    refactor: Core 도메인의 GameSession using 제거

--- Assets/Scripts/Core/Domain/Domain.cs
+++ Assets/Scripts/Core/Domain/Domain.cs
@@ -2,8 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Yujanggi.Core.Match;
-using Yujanggi.Runtime.GameSession;
+
 namespace Yujanggi.Core.Domain
 {
     public interface IGameResultContext
